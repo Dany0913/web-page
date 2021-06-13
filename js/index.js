ecf0f1a19e1a69f422076a9fc754d95ed438bb37
Daniela <dany0913@hotmail.com>
2021-06-13 13:53:25 -0500

    nav responsive functional, different CSS in progress

---
+++ js/index.js
@@ -0,0 +1,14 @@
+let navBurguer = document.getElementsByClassName("header-nav-burguer")[0];
+let navMenu = document.getElementsByClassName("header-nav")[0];
+console.log(navMenu);
+
+function fnClicNavBurguer() {
+  console.log("Clic en burguer " + navMenu.className);
+  if (navMenu.className === "header-nav show") {
+    return (navMenu.className = "header-nav");
+  } else {
+    return (navMenu.className += " show");
+  }
+}
+
+navBurguer.addEventListener("click", fnClicNavBurguer);
